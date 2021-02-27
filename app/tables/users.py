d150a125a0ea46cc73f900ab0b16b34d5fb7e06d
agusrichard <agus.richard21@gmail.com>
2021-02-27 14:12:24 +0700

    Create migration and migrate

--- app/tables/users.py
+++ app/tables/users.py
@@ -6,6 +6,14 @@ users = sqlalchemy.Table(
     metadata,
     sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column('email', sqlalchemy.String(256), unique=True),
+    sqlalchemy.Column('password', sqlalchemy.String(256)),
     sqlalchemy.Column('username', sqlalchemy.String(64), nullable=True),
     sqlalchemy.Column('fullname', sqlalchemy.String(128), nullable=True),
+
+    # foreign key(s)
+    sqlalchemy.Column('role_id', sqlalchemy.Integer),
+    sqlalchemy.ForeignKeyConstraint(
+        ['role_id'], ['roles.id'],
+        name='fk_user_role'
+    )
 )
