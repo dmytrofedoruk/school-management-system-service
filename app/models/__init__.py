01b2d50cf8cf5402b0311091765676de2b3be20b
agusrichard <agus.richard21@gmail.com>
2021-03-06 14:37:31 +0700

    Working on classrooms path operations

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .accounts import User as UserModel, Role as RoleModel
 from .departements import Departement as DepartementModel
 from .faculties import Faculty as FacultyModel
+from .classrooms import Classroom as ClassroomModel
