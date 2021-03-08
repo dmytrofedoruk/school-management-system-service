0bbcde7fbb47db1cfec387f913c33aa87caea0ea
agusrichard <agus.richard21@gmail.com>
2021-03-08 22:14:22 +0700

    Success to create admin with new role mappings

--- app/admin.py
+++ app/admin.py
@@ -6,17 +6,12 @@ from .models import UserModel
 from .schemas import UserRegisterRequest, RoleEnum
 
 def create_admin(user: UserRegisterRequest):
-    result: any = None
     engine = create_engine(Envs.DATABASE_URL)
 
     with engine.connect() as connection:
         with connection.begin():
             user.password = UserModel.pwd_context.hash(user.password)
-            user_insert_query = users.insert().values(**user.dict())
-            print('user_insert_query', user_insert_query)
-            user_id = connection.execute(user_insert_query)
-            print('user_id', user_id)
+            user_insert_query = users.insert().values(**user.dict()).returning(users.c.id)
+            user_id, = connection.execute(user_insert_query).fetchone()
             role_insert_query = users_roles.insert()
-            result = connection.execute(role_insert_query, [{'user_id': user_id, 'role_id': RoleEnum.ADMIN}])
-        
-    return result
+            connection.execute(role_insert_query, [{'user_id': user_id, 'role_id': RoleEnum.ADMIN}])
