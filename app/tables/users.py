090facc05f4ae8239ddade56de4dbd91699fa685
agusrichard <agus.richard21@gmail.com>
2021-03-08 15:08:41 +0700

    Defining user role mappings

--- app/tables/users.py
+++ app/tables/users.py
@@ -12,12 +12,13 @@ users = sqlalchemy.Table(
     sqlalchemy.Column('username', sqlalchemy.String(64), nullable=True),
     sqlalchemy.Column('fullname', sqlalchemy.String(128), nullable=True),
     sqlalchemy.Column('created_at', sqlalchemy.DateTime, server_default=sqlalchemy.func.now()),
-    sqlalchemy.Column('modified_at', sqlalchemy.DateTime, server_default=sqlalchemy.func.now(), onupdate=sqlalchemy.func.now()),
+    sqlalchemy.Column('modified_at', sqlalchemy.DateTime, server_default=sqlalchemy.func.now(), onupdate=sqlalchemy.func.now())
+)
 
-    # foreign key(s)
-    sqlalchemy.Column('role_id', sqlalchemy.Integer),
-    sqlalchemy.ForeignKeyConstraint(
-        ['role_id'], ['roles.id'],
-        name='fk_user_role'
-    )
+users_roles = sqlalchemy.Table(
+    'user_role_mappings',
+    metadata,
+    sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column('user_id', sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id')),
+    sqlalchemy.Column('role_id', sqlalchemy.Integer, sqlalchemy.ForeignKey('roles.id'))
 )
