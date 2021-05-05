3b0da4d67ba3e887709e98d9fe107ac368441019
agusrichard <agus.richard21@gmail.com>
2021-05-05 13:47:27 +0700

    feat: new column of is_validated on users table

--- app/tables/users.py
+++ app/tables/users.py
@@ -1,5 +1,6 @@
 import sqlalchemy
-from datetime import datetime 
+from sqlalchemy.sql import expression
+from datetime import datetime
 
 from ..config.db import db, metadata
 
@@ -11,14 +12,20 @@ users = sqlalchemy.Table(
     sqlalchemy.Column('password', sqlalchemy.String(256)),
     sqlalchemy.Column('username', sqlalchemy.String(64), nullable=True),
     sqlalchemy.Column('fullname', sqlalchemy.String(128), nullable=True),
-    sqlalchemy.Column('created_at', sqlalchemy.DateTime, server_default=sqlalchemy.func.now()),
-    sqlalchemy.Column('modified_at', sqlalchemy.DateTime, server_default=sqlalchemy.func.now(), onupdate=sqlalchemy.func.now())
+    sqlalchemy.Column('is_validated', sqlalchemy.Boolean, nullable=False,
+                      server_default=expression.false()),
+    sqlalchemy.Column('created_at', sqlalchemy.DateTime,
+                      server_default=sqlalchemy.func.now()),
+    sqlalchemy.Column('modified_at', sqlalchemy.DateTime,
+                      server_default=sqlalchemy.func.now(), onupdate=sqlalchemy.func.now())
 )
 
 users_roles = sqlalchemy.Table(
     'user_role_mappings',
     metadata,
     sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column('user_id', sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id')),
-    sqlalchemy.Column('role_id', sqlalchemy.Integer, sqlalchemy.ForeignKey('roles.id'))
+    sqlalchemy.Column('user_id', sqlalchemy.Integer,
+                      sqlalchemy.ForeignKey('users.id')),
+    sqlalchemy.Column('role_id', sqlalchemy.Integer,
+                      sqlalchemy.ForeignKey('roles.id'))
 )
---
+++ sql/roles.sql
@@ -0,0 +1,7 @@
+INSERT INTO roles(title, description)
+VALUES
+    ('ADMIN', 'Admin'),
+    ('FACULTY_DEAN', 'Faculty Dean'),
+    ('HEAD_DEPARTEMENT', 'Head Departement'),
+    ('TEACHER', 'Teacher'),
+    ('STUDENT', 'Student')
